859662857473e2c03c926ca9a78e99d07e56f236
Shehnazdeep Kaur <kaur6200@sheridancollege.ca>
2023-11-25 22:36:37 -0500

    Get Random Images Added

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -111,6 +111,95 @@ namespace API.Controllers
 
         }
 
+        // GET /api/images/byTag?tag=cars&pageNumber=1&pageSize=10
+        [HttpGet("byTag")]
+        public async Task<IActionResult> GetImagesByTag([FromQuery] string tag, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+
+
+
+            if (!string.IsNullOrEmpty(tag))
+            {
+
+
+                var result = await _db.Images
+     .Include(image => image.Tags)
+     .Include(x => x.User)
+     .Where(image => image.Tags.Any(t => t.Text == tag))
+     .OrderByDescending(image => image.PostingDate)
+     .Skip((pageNumber - 1) * pageSize)
+     .Take(pageSize)
+     .ToListAsync();
+
+
+
+
+
+                var totalImages = await _db.Images.CountAsync();
+                var totalPages = (int)Math.Ceiling((double)totalImages / pageSize);
+
+                var imageDtos = result.Select(image => new ImageDTO
+                {
+                    Id = image.Id,
+                    Url = image.Url,
+                    Username = image.User?.Name, // Accessing the Name property inside the User
+
+                }).ToList();
+
+                var response = new PageResponse<ImageDTO>(imageDtos);
+                //   var totalRecords = _db.People.CountAsync();
+                response.Meta.Add("TotalPages", totalPages);
+                response.Meta.Add("TotalRecords", totalImages);
+                var links = LinksGenerator.GenerateLinks("/api/Images", pageNumber, totalImages, pageSize);
+
+                response.Links = links;
+                return Ok(response);
+
+            }
+            else
+            {
+
+                return NotFound(new
+                {
+                    Message = "Tag does not exists."
+                });
+            }
+
+
+
+
+
+        }
+        [HttpGet("random")]
+        public async Task<IActionResult> GetRandomImages()
+        {
+            var totalImages = await _db.Images.CountAsync();
+            var count = 2;
+
+            var result = await _db.Images.Include(x => x.User).ToListAsync();
+
+            // Order by a random GUID to introduce randomness
+            var orderedResult = result.OrderBy(image => Guid.NewGuid()).Take(count).ToList();
+
+            var imageDtos = orderedResult.Select(image => new ImageDTO
+            {
+                Id = image.Id,
+                Url = image.Url,
+                Username = image.User?.Name,
+            }).ToList();
+
+            var totalPages = (int)Math.Ceiling((double)totalImages / count);
+            var response = new PageResponse<ImageDTO>(imageDtos);
+            response.Meta.Add("TotalPages", totalPages);
+            response.Meta.Add("TotalRecords", totalImages);
+            var links = LinksGenerator.GenerateLinks("/api/Images", 1, totalImages, 10);
+
+            response.Links = links;
+            return Ok(response);
+        }
+
+
+
 
     }
 }
